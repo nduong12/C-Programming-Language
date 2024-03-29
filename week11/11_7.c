00508b8c6b6dfe2af76219ee38df3033428a37b6
nduong12 <nduongg6@gmail.com>
2024-03-29 08:45:32 +0700

    Bai tap

---
+++ week11/11_7.c
@@ -0,0 +1,51 @@
+/*#include <stdio.h>
+
+void tangLuong(int* luong, int namBatDau, int namHienTai) {
+    int soNamLamViec = namHienTai - namBatDau;
+    int soLanTangLuong = soNamLamViec / 3;
+    for (int i = 0; i < soLanTangLuong; i++) {
+        *luong += *luong / 100;
+    }
+}
+
+int main() {
+    int N = 5;
+    int luong[N] = {1000, 2000, 3000, 4000, 5000};
+    int namBatDau[N] = {2010, 2011, 2012, 2013, 2014};
+    int namHienTai = 2024;
+
+    for (int i = 0; i < N; i++) {
+        tangLuong(&luong[i], namBatDau[i], namHienTai);
+        printf("Luong cua nhan vien %d sau khi tang la: %d\n", i+1, luong[i]);
+    }
+
+    return 0;
+}*/
+
+/*#include <stdio.h>
+
+void tangLuong(int* luong, int namBatDau, int namHienTai) {
+    int soNamLamViec = namHienTai - namBatDau;
+    int soLanTangLuong = soNamLamViec / 3;
+    for (int i = 0; i < soLanTangLuong; i++) {
+        *luong += *luong / 100;
+    }
+}
+
+int main() {
+    int N = 5;
+    int luong[N] = {1000, 2000, 3000, 4000, 5000};
+    int namBatDau[N] = {2010, 2011, 2012, 2013, 2014};
+    int namHienTai = 2024;
+
+    for (int i = 0; i < N; i++) {
+        tangLuong(&luong[i], namBatDau[i], namHienTai);
+        printf("Luong cua nhan vien %d sau khi tang la: %d\n", i+1, luong[i]);
+    }
+
+    return 0;
+}*/
+
+
+
+
